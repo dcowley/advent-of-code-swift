078fea7ec1c09efc99507fb43ed462aef1c32fce
Dean Cowley <dean.w.cowley@gmail.com>
2025-01-26 17:13:53 +0000

    Day 01: Not Quite Lisp (refactor)

--- 2015/Tests/Day01Tests.swift
+++ 2015/Tests/Day01Tests.swift
@@ -3,28 +3,27 @@ import Testing
 @testable import AoC2015
 
 struct Day01Tests {
-  let testData = [
-    "(())": 0,
-    "()()": 0,
-    "(((": 3,
-    "(()(()(": 3,
-    "))(((((": 3,
-    "())": -1,
-    "))(": -1,
-    ")))": -3,
-    ")())())": -3,
-  ]
-  
   @Test func testPart1() {
+    let testData = [
+      "(())": 0,
+      "()()": 0,
+      "(((": 3,
+      "(()(()(": 3,
+      "))(((((": 3,
+      "())": -1,
+      "))(": -1,
+      ")))": -3,
+      ")())())": -3,
+    ]
+
     testData.forEach { input, expected in
       let challenge = Day01(data: input)
       #expect(challenge.part1() == expected)
     }
   }
-  
+
   @Test func testPart2() {
     #expect(Day01(data: ")").part2() == 1)
-    debugPrint(Day01(data: "()())").part2())
     #expect(Day01(data: "()())").part2() == 5)
   }
 }
