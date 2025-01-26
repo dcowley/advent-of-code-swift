654cdcb406bb36492b18b80fc394e4c5341fe159
Dean Cowley <dean.w.cowley@gmail.com>
2025-01-26 17:13:53 +0000

    Day 01: Not Quite Lisp (refactor)

--- 2015/Sources/Day01.swift
+++ 2015/Sources/Day01.swift
@@ -2,19 +2,20 @@ import Foundation
 
 struct Day01: AdventDay {
   var data: String
-  
+
   func part1() -> Int {
-    return data.count(where: { $0 == "(" }) - data.count(where: { $0 == ")" })
+    return data.reduce(0, { $0 + ($1 == "(" ? 1 : -1 )})
   }
-  
+
   func part2() -> Int {
     var chars = data
 
-    var index = 0
     var floor = 0
+    var index = 0
+
     while (floor != -1) {
       let char = chars.removeFirst()
-      floor = char == "(" ? floor + 1 : floor - 1
+      floor += char == "(" ? 1 : -1
       index += 1
     }
     return index
