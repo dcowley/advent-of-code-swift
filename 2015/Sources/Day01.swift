096a82a605c3027497e70cb520a46ae99ca62ebc
Dean Cowley <dean.w.cowley@gmail.com>
2025-01-26 17:10:42 +0000

    Day 01: Not Quite Lisp (part 2)

--- 2015/Sources/Day01.swift
+++ 2015/Sources/Day01.swift
@@ -6,4 +6,17 @@ struct Day01: AdventDay {
   func part1() -> Int {
     return data.count(where: { $0 == "(" }) - data.count(where: { $0 == ")" })
   }
+  
+  func part2() -> Int {
+    var chars = data
+
+    var index = 0
+    var floor = 0
+    while (floor != -1) {
+      let char = chars.removeFirst()
+      floor = char == "(" ? floor + 1 : floor - 1
+      index += 1
+    }
+    return index
+  }
 }
