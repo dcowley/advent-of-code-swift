f4cc9b87e496b12dd7d43f9d003a7ee033d17b00
Dean Cowley <dean.w.cowley@gmail.com>
2025-01-27 16:49:13 +0000

    Day 04: The Ideal Stocking Stuffer (part 1)

---
+++ 2015/Tests/Day04Tests.swift
@@ -0,0 +1,10 @@
+import Testing
+
+@testable import AoC2015
+
+struct Day04Tests {
+  @Test func testPart1() {
+    #expect(Day04(data: "abcdef").part1() == 609043)
+    #expect(Day04(data: "pqrstuv").part1() == 1048970)
+  }
+}
