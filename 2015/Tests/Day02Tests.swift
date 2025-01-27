cfe1df20b874727051e638fc55a99a959b07188d
Dean Cowley <dean.w.cowley@gmail.com>
2025-01-27 09:13:27 +0000

    Day 02: I Was Told There Would Be No Math (part 2)

--- 2015/Tests/Day02Tests.swift
+++ 2015/Tests/Day02Tests.swift
@@ -7,4 +7,9 @@ struct Day02Tests {
     #expect(Day02(data: "2x3x4").part1() == 58)
     #expect(Day02(data: "1x1x10").part1() == 43)
   }
+
+  @Test func testPart2() {
+    #expect(Day02(data: "2x3x4").part2() == 34)
+    #expect(Day02(data: "1x1x10").part2() == 14)
+  }
 }
