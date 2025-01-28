a72b764ffa47ca64945c976b2840dd32eb73174c
Dean Cowley <dean.w.cowley@gmail.com>
2025-01-28 13:34:25 +0000

    Day 05: Doesn't He Have Intern-Elves For This? (part 2)

--- 2015/Sources/Day05.swift
+++ 2015/Sources/Day05.swift
@@ -6,12 +6,28 @@ struct Day05: AdventDay {
   var data: String
 
   func part1() throws -> Int {
-    let vowels = try Regex("^(?:.*[aeiou].*){3}$")
-    let repeats = try Regex("(.)\\1")
-    let allowed = try Regex("^(?!.*(ab|cd|pq|xy)).*$")
+    let rule1 = try Regex("(?:.*[aeiou].*){3}")
+    let rule2 = try Regex("(.)\\1")
+    let rule3 = try Regex("^(?!.*(ab|cd|pq|xy)).*$")
 
     return data.split(separator: "\n").count {
-      !$0.matches(of: vowels).isEmpty && !$0.matches(of: repeats).isEmpty && !$0.matches(of: allowed).isEmpty
+      !(
+        $0.matches(of: rule1).isEmpty ||
+        $0.matches(of: rule2).isEmpty ||
+        $0.matches(of: rule3).isEmpty
+      )
+    }
+  }
+
+  func part2() throws -> Int {
+    let rule1 = try Regex("(.)(.)(?:.*)\\1\\2")
+    let rule2 = try Regex("(.).\\1")
+
+    return data.split(separator: "\n").count {
+      !(
+        $0.matches(of: rule1).isEmpty ||
+        $0.matches(of: rule2).isEmpty
+      )
     }
   }
 }
