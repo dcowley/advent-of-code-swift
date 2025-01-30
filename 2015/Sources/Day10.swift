b4b0a9497997c91af869383103f3ad995f7588a5
Dean Cowley <dean.w.cowley@gmail.com>
2025-01-30 15:51:57 +0000

    Day 10: Elves Look, Elves Say (part 2)

--- 2015/Sources/Day10.swift
+++ 2015/Sources/Day10.swift
@@ -3,12 +3,24 @@ import Foundation
 struct Day10: AdventDay {
   var data: String
 
+  func next(_ input: String) -> String {
+    input.matches(of: /(.)\1*/)
+      .map { "\($0.output.0.count)\($0.output.1)" }
+      .joined()
+  }
+
   func part1() -> Int {
     var text = data.trimmingCharacters(in: .newlines)
     for _ in 0..<40 {
-      text = text.matches(of: /(.)\1*/)
-        .map { "\($0.output.0.count)\($0.output.1)" }
-        .joined()
+      text = next(text)
+    }
+    return text.count
+  }
+
+  func part2() -> Int {
+    var text = data.trimmingCharacters(in: .newlines)
+    for _ in 0..<50 {
+      text = next(text)
     }
     return text.count
   }
--- 2015/Tests/Day10Tests.swift
+++ 2015/Tests/Day10Tests.swift
@@ -3,4 +3,10 @@ import Testing
 @testable import AoC2015
 
 struct Day10Tests {
+  @Test func testNext() {
+    #expect(Day10().next("1") == "11")
+    #expect(Day10().next("11") == "21")
+    #expect(Day10().next("1211") == "111221")
+    #expect(Day10().next("111221") == "312211")
+  }
 }
