eb9d94e9f7922ce8660ef8c2f165d3c1f0235016
Dean Cowley <dean.w.cowley@gmail.com>
2025-02-03 17:03:39 +0000

    Day 12: JSAbacusFramework.io (part 2)

--- 2015/Sources/Day12.swift
+++ 2015/Sources/Day12.swift
@@ -8,4 +8,32 @@ struct Day12: AdventDay {
       .map { Int($0.output.num)! }
       .reduce(0, +)
   }
+
+  func part2() throws -> Int {
+    func solve(_ json: Any) -> Int {
+      if let num = json as? Int {
+        return num
+      }
+
+      if let arr = json as? [Any] {
+        return arr.reduce(0) {
+          $0 + solve($1)
+        }
+      }
+
+      if let obj = json as? [String: Any] {
+        if obj.values.contains(where: { $0 as? String == "red" }) {
+          return 0
+        }
+
+        return obj.values.reduce(0) {
+          $0 + solve($1)
+        }
+      }
+
+      return 0
+    }
+
+    return solve(try JSONSerialization.jsonObject(with: data.data(using: .utf8)!))
+  }
 }
