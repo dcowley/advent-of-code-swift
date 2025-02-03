addee2958cb182152246db050ed49cfb4909a44a
Dean Cowley <dean.w.cowley@gmail.com>
2025-02-03 17:03:39 +0000

    Day 12: JSAbacusFramework.io (part 2)

--- 2015/Tests/Day12Tests.swift
+++ 2015/Tests/Day12Tests.swift
@@ -11,4 +11,11 @@ struct Day12Tests {
     #expect(Day12(data: "{\"a\":[-1,1]}").part1() == 0)
     #expect(Day12(data: "[-1,{\"a\":1}]").part1() == 0)
   }
+
+  @Test func testPart2() throws {
+    #expect(try Day12(data: "[1,2,3]").part2() == 6)
+    #expect(try Day12(data: "[1,{\"c\":\"red\",\"b\":2},3]").part2() == 4)
+    #expect(try Day12(data: "{\"d\":\"red\",\"e\":[1,2,3,4],\"f\":5}").part2() == 0)
+    #expect(try Day12(data: "[1,\"red\",5]").part2() == 6)
+  }
 }
