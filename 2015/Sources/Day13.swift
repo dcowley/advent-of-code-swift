a80a44cd6b577947eef059cb3ac3b33aedcf04df
Dean Cowley <dean.w.cowley@gmail.com>
2025-02-04 21:23:20 +0000

    Day 13: Knights of the Dinner Table (part 1)

---
+++ 2015/Sources/Day13.swift
@@ -0,0 +1,59 @@
+import Foundation
+
+struct Day13: AdventDay {
+  var data: String
+
+  private enum Effect : Substring {
+    case gain
+    case lose
+  }
+
+  private struct Pair: Hashable {
+    let guest1: String
+    let guest2: String
+
+    init(guests: (String, String)) {
+      guest1 = guests.0
+      guest2 = guests.1
+    }
+  }
+
+  private var relationships: [String: [String: Int]] {
+    var relationships: [String: [String: Int]] = [:]
+    data.split(separator: "\n").forEach { line in
+      if let match = line.wholeMatch(of: /^(?'guest1'\w+)(?:[\w ])*(?'effect'gain|lose) (?'units'\d+)(?:[\w ])*(?: (?'guest2'\w+))\.$/) {
+        let guest1 = String(match.output.guest1)
+        let guest2 = String(match.output.guest2)
+        let units = Int(match.output.units)!
+        let effect = Effect(rawValue: match.output.effect)!
+
+        let change = switch (effect) {
+        case .gain: units
+        case .lose: -units
+        }
+
+        relationships[guest1, default: [:]][guest2] = change
+      }
+    }
+    return relationships
+  }
+
+  func part1() -> Int {
+    let permutations = relationships.keys.permutations(ofCount: relationships.keys.count)
+
+    var cache: Dictionary<Pair, Int> = [:]
+
+    return permutations.map {
+      ($0 + $0.prefix(1)).adjacentPairs().reduce(0) {
+        let key = Pair(guests: $1)
+        if let cached = cache[key] {
+          return $0 + cached
+        }
+
+        let change = relationships[$1.0]![$1.1]! + relationships[$1.1]![$1.0]!
+        cache[key] = change
+        return $0 + change
+      }
+    }.max()!
+  }
+}
