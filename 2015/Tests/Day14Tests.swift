ca106aa7a3129bae82252d4e7d6bc149acb98e23
Dean Cowley <dean.w.cowley@gmail.com>
2025-02-05 10:26:07 +0000

    Day 14: Reindeer Olympics (part 1)

---
+++ 2015/Tests/Day14Tests.swift
@@ -0,0 +1,17 @@
+import Testing
+
+@testable import AoC2015
+
+struct Day14Tests {
+  private let testData = """
+  Comet can fly 14 km/s for 10 seconds, but then must rest for 127 seconds.
+  Dancer can fly 16 km/s for 11 seconds, but then must rest for 162 seconds.
+  """.replacing(/^ +/.anchorsMatchLineEndings(), with: { _ in "" })
+
+  @Test func testPart1() {
+    #expect(Day14(data: testData, totalTime: 1).part1() == 16)
+    #expect(Day14(data: testData, totalTime: 10).part1() == 160)
+    #expect(Day14(data: testData, totalTime: 12).part1() == 176)
+    #expect(Day14(data: testData, totalTime: 1000).part1() == 1120)
+  }
+}
