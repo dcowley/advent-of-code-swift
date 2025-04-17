de37a56773c2691660d71b86175e4bbd80440ac2
Dean Cowley <dean.w.cowley@gmail.com>
2025-04-17 06:38:45 +0100

    Day 01: No Time for a Taxicab (part 2)

--- 2016/Sources/Day01.swift
+++ 2016/Sources/Day01.swift
@@ -9,20 +9,45 @@ private struct Instruction {
 struct Day01: AdventDay {
   var data: String
 
-  func part1() throws -> Int {
-    let instructions: [Instruction] = data.matches(of: /(?'turn'[R|L])(?'steps'\d+)/)
+  private var instructions: [Instruction] {
+    data.matches(of: /(?'turn'[R|L])(?'steps'\d+)/)
       .map {
         Instruction(
           rotation: $0.output.turn == "R" ? .clockwise : .anticlockwise,
           steps: Int($0.output.steps)!
         )
       }
+  }
 
+  func part1() throws -> Int {
     var direction = Direction.north
     var point = Point(x: 0, y: 0)
+
     instructions.forEach {
       direction = direction.turn(rotation: $0.rotation)
       point = point.move(direction: direction, steps: $0.steps)
+    }
+
+    return abs(point.x) + abs(point.y)
+  }
+
+  func part2() throws -> Int {
+    var direction = Direction.north
+    var point = Point(x: 0, y: 0)
+
+    var visitedPoints: Set<Point> = []
+    for instruction in instructions {
+      direction = direction.turn(rotation: instruction.rotation)
+
+      for _ in 0..<instruction.steps {
+        point = point.move(direction: direction)
+
+        if visitedPoints.contains(point) {
+          return abs(point.x) + abs(point.y)
+        } else {
+          visitedPoints.insert(point)
+        }
+      }
     }
 
     return abs(point.x) + abs(point.y)
--- 2016/Tests/Day01Tests.swift
+++ 2016/Tests/Day01Tests.swift
@@ -8,4 +8,8 @@ struct Day01Tests {
     #expect(try Day01(data: "R2, R2, R2").part1() == 2)
     #expect(try Day01(data: "R5, L5, R5, R3").part1() == 12)
   }
+
+  @Test func testPart2() throws {
+    #expect(try Day01(data: "R8, R4, R4, R8").part2() == 4)
+  }
 }
