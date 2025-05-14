077ed6cff561ae16b939462e8ef9c92fd8d878c8
Dean Cowley <dean.w.cowley@gmail.com>
2025-05-14 15:55:07 +0100

    Day 03: Squares With Three Sides (refactor)

--- 2016/Sources/Day03.swift
+++ 2016/Sources/Day03.swift
@@ -1,20 +1,17 @@
+import Algorithms
 import Foundation
 import Shared
 
 struct Day03: AdventDay {
   let data: String
 
-  private var regex: Regex<(Substring, Substring, Substring, Substring)> {
-    /\s+(\d+)\s+(\d+)\s+(\d+)/.anchorsMatchLineEndings()
-  }
-
   func part1() throws -> Int {
-    data.matches(of: regex).count { match in
-      let (_, x, y, z) = match.output
-      let sides = [Int(x)!, Int(y)!, Int(z)!].sorted()
+    let sides =  data.matches(of: /(\d+)/)
+      .compactMap { Int($0.output.0) }
+      .chunks(ofCount: 3)
+      .map { $0.sorted() }
 
-      return sides[0] + sides[1] > sides[2]
-    }
+    return sides.count { $0[0] + $0[1] > $0[2] }
   }
 
   func part2() throws -> Int {
