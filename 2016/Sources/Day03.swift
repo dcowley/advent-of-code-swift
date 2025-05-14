76a7a46d66277ea43982fab0c846b66081a0e475
Dean Cowley <dean.w.cowley@gmail.com>
2025-05-14 15:38:09 +0100

    Day 03: Squares With Three Sides (part 2)

--- 2016/Sources/Day03.swift
+++ 2016/Sources/Day03.swift
@@ -16,4 +16,25 @@ struct Day03: AdventDay {
       return sides[0] + sides[1] > sides[2]
     }
   }
+
+  func part2() throws -> Int {
+    let numbers = data.matches(of: /(\d+)/).compactMap { Int($0.output.0) }
+
+    var count = 0
+    for i in 0..<3 {
+      let indices = sequence(first: i) {
+        guard numbers.indices.contains($0 + 3) else {
+          return nil
+        }
+        return $0 + 3
+      }
+
+      count += indices.map { numbers[$0] }
+        .chunks(ofCount: 3)
+        .map { $0.sorted() }
+        .count { $0[0] + $0[1] > $0[2] }
+    }
+
+    return count
+  }
 }
