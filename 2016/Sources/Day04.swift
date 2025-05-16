8e42a1484abc2c150c12f35f669c60478341fb1d
Dean Cowley <dean.w.cowley@gmail.com>
2025-05-16 08:35:24 +0100

    Day 04: Security Through Obscurity (part 2)

--- 2016/Sources/Day04.swift
+++ 2016/Sources/Day04.swift
@@ -29,4 +29,26 @@ struct Day04: AdventDay {
         }
       }
   }
+
+  func part2() -> Int {
+    let asciiStart = Int(Character("a").asciiValue!)
+
+    return data.matches(of: /([a-z-]+)-(\d+)\[([a-z]+)]/)
+      .map { match in
+        let (_, name, id, _) = match.output
+
+        let decrypted = name.split(separator: "-")
+          .map { word in
+            String(
+              word.map { letter in
+                Character(UnicodeScalar((Int(letter.asciiValue!) - asciiStart + Int(id)!) % 26 + asciiStart)!)
+              }
+            )
+          }
+          .joined(separator: " ")
+
+        return (decrypted, Int(id)!)
+      }
+      .first(where: { $0.0 == "northpole object storage" })!.1
+  }
 }
