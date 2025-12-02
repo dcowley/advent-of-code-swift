6ab126257f61513b48cb3c45c29cf3483661167a
Dean Cowley <dean.w.cowley@gmail.com>
2025-12-02 19:28:39 +0000

    🧩 2025/01 Secret Entrance

--- 2025/Tests/Day01Tests.swift
+++ 2025/Tests/Day01Tests.swift
@@ -4,13 +4,23 @@ import Testing
 
 struct Day01Tests {
   private let testData = """
+  L68
+  L30
+  R48
+  L5
+  R60
+  L55
+  L1
+  L99
+  R14
+  L82
   """
 
   @Test func testPart1() throws {
-    #expect(try Day01(data: testData).part1() == Int.max)
+    #expect(try Day01(data: testData).part1() == 3)
   }
 
   @Test func testPart2() throws {
-    #expect(try Day01(data: testData).part2() == Int.max)
+    #expect(try Day01(data: testData).part2() == 6)
   }
 }
